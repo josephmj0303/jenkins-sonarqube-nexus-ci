844cafff3a10dcce3b81dbe6b7e207b79128d86d
josephmj0303 <joseph.mj@aol.com>
2026-05-18 13:06:04 +0000

    Fix Jenkins userdata bootstrap (#1)

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 sudo apt update
- 
-sudo apt install openjdk-17-jdk -y
- 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
- 
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+sudo apt install -y openjdk-21-jdk wget
+
+sudo install -d -m 0755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian-stable binary/" | sudo tee \
 /etc/apt/sources.list.d/jenkins.list > /dev/null
- 
-sudo apt-get update
- 
-sudo apt-get install jenkins -y
+
+sudo apt update
+sudo apt install -y jenkins
+sudo systemctl enable --now jenkins
